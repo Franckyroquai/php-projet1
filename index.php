df9de23bd7478207ba71d5193fa09fa687e9b274
Franckyroquai <franckyroquai@gmail.com>
2024-02-18 16:11:43 +0100

    les variables en php

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-projet1</title>
 </head>
+<title>
+    <?php
+       echo "<h1>Zou, Hello World !</h1>";
+    ?>
+</title>
 <body>
-    
+    <?php
+       $nom = "Franck";
+       $age = 47;
+    ?>
+    Mon nom est <?php echo $nom; ?>, et mon age est <?php echo $age; ?> ans.
 </body>
 </html>
