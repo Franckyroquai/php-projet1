427a1a8903fea8e2e43095bde9b4171afcd2071a
Franckyroquai <franckyroquai@gmail.com>
2024-03-06 17:42:15 +0100

    Les Cookies

--- test.php
+++ test.php
@@ -10,3 +10,28 @@ unset($_SESSION); // détruit la variable
 //var_dump($_SESSION);
 
 ?>
+
+<?php
+        // $temps_cookie = 3600*24*365;
+        // setcookie("cookie[user]", "Jean", time()+$temps_cookie);
+        // setcookie("cookie[age]", 45, time()+$temps_cookie);
+        // setcookie("cookie[produit]", "Chemises", time()+$temps_cookie);
+
+        // echo $_COOKIE["user"];
+
+        // if(isset($_COOKIE["cookie"])){
+        //     foreach ($_COOKIE["cookie"] as $key => $value) {
+        //         echo "clé : ".$key."Valeur : ".$value."<br>";
+        //     }
+        // }
+
+        $temps_cookie = 3600*24*365;
+    $valeur_cookie = array("user" => "Jean", "age"=> 45, "produit"=> "Chemise");
+    $valeur = serialize($valeur_cookie);
+    setcookie("serialise", $valeur, time() + $temps_cookie,"/");
+    if (isset($_COOKIE["serialise"])){
+        $tableau_cookie = unserialize($_COOKIE["serialise"]);
+        var_dump($tableau_cookie);
+    }
+        
+        ?>
